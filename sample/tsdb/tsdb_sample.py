29cd49e65632b8967b490bcaaf38d52bcbdac243
chensheng06 <chensheng06@baidu.com>
2019-05-08 17:19:56 +0800

    finish tsdb py2_py3 bce-iot-8871
    Change-Id: I60e7f5cd8ed486e178b1e89748c747e2476e6467
    
    finish tsdb py2_py3 bce-iot-8871

--- sample/tsdb/tsdb_sample.py
+++ sample/tsdb/tsdb_sample.py
@@ -18,8 +18,9 @@ import random
 import string
 import time
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+if sys.version_info[0] == 2 :
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
 import tsdb_sample_conf
 from baidubce.exception import BceServerError
@@ -42,7 +43,7 @@ if __name__ == "__main__":
     #            write operation samples
     ######################################################################################################
 
-    #write single field
+    # write single field
     datapoints = [{
         "metric": "wind",
         "tags": {
@@ -54,11 +55,11 @@ if __name__ == "__main__":
         "value": 1
     }]
     try:
-        print tsdb_client.write_datapoints(datapoints)  #if write fail, it will throw a exception
+        print(tsdb_client.write_datapoints(datapoints))
     except BaseException as e:
-        print e
+        print(e)
 
-    #write multiple fields
+    # write multiple fields
     datapoints = [{
         "metric": "wind",
         "tags": {
@@ -80,27 +81,27 @@ if __name__ == "__main__":
     }]
 
     try:
-        print tsdb_client.write_datapoints(datapoints)
+        print(tsdb_client.write_datapoints(datapoints)) 
     except BaseException as e:
-        print e
+        print(e)
 
-    ######################################################################################################
-    #            query operation samples
-    ######################################################################################################
+    # ######################################################################################################
+    # #            query operation samples
+    # ######################################################################################################
 
     # get metrics
     result = tsdb_client.get_metrics()
-    print result.metrics
+    print(result.metrics)
 
     # get fields
-    result = tsdb_client.get_fields('wind')
-    print result.fields
+    result = tsdb_client.get_fields(b'wind')
+    print(result.fields)
 
     # get tags
-    result = tsdb_client.get_tags('wind')
-    print result.tags
+    result = tsdb_client.get_tags(b'wind')
+    print(result.tags)
 
-    # single field query datapoints
+    #single field query datapoints
     query_list = [{
         "metric": "wind",
         "field": "direction",
@@ -123,7 +124,7 @@ if __name__ == "__main__":
         }]
     }]
     result = tsdb_client.get_datapoints(query_list)
-    print result.results
+    print(result.results)
 
     # multiple fields query datapoints
     query_list = [{
@@ -148,7 +149,7 @@ if __name__ == "__main__":
         }]
     }]
     result = tsdb_client.get_datapoints(query_list)
-    print result.results
+    print(result.results)
 
     # query datapoints with fill
     query_list = [{
@@ -179,7 +180,7 @@ if __name__ == "__main__":
     }]
 
     result = tsdb_client.get_datapoints(query_list)
-    print result.results
+    print(result.results)
 
     # query datapoints with partition page
     query_list = [{
@@ -199,9 +200,9 @@ if __name__ == "__main__":
         count += 1
         if len(query_list) > 0:
             result = tsdb_client.get_datapoints(query_list)
-            print count, result.results
+            print(count, result.results)
         else:
-            print 'end query'
+            print('end query')
             break
         next_query = []
         for i in range(len(query_list)):
@@ -211,24 +212,24 @@ if __name__ == "__main__":
         query_list = next_query
 
     # query datapoints with sql
-    sql = "select * from wind" 
+    sql = b"select * from wind" 
     result = tsdb_client.get_rows_with_sql(sql)
-    print result
+    print(result)
 
     # generate pre signed url sample
     timestamp = int(time.time())
     per_signed_url = tsdb_client.generate_pre_signed_url(query_list,
             timestamp=timestamp, expiration_in_seconds=1800)
-    print per_signed_url
+    print(per_signed_url)
 
     # generate pre signed url with sql sample
-    sql = "select * from wind"
+    sql = b"select * from wind"
     timestamp = int(time.time())
     per_signed_url_sql = tsdb_client.generate_pre_signed_url_with_sql(sql,
             timestamp=timestamp, expiration_in_seconds=1800)
-    print per_signed_url_sql
+    print(per_signed_url_sql)
 
     # write datapoint don't use gzip compress
     response = tsdb_client.write_datapoints(datapoints, use_gzip=False)
-    print response
+    print(response)
 
