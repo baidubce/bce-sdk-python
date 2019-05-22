01704e35ba854365572e430b064f574efc46dc48
weizhijun <weizhijun01@baidu.com>
2019-05-22 10:23:59 +0800

    "解决合入冲突"

--- test/dumap/test_dumap_client.py
+++ test/dumap/test_dumap_client.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+# Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 # except in compliance with the License. You may obtain a copy of the License at
@@ -19,6 +19,8 @@ import unittest
 
 from baidubce.services.dumap import dumap_client
 import dumap_test_config
+
+import json
 
 
 class TestDumapClient(unittest.TestCase):
@@ -98,6 +100,61 @@ class TestDumapClient(unittest.TestCase):
             params=params)
         print(response)
 
+    def test_locate_ip(self):
+        """
+        test locate ip
+        """
+
+        params = {}
+        params['ip'] = ""
+
+        response = self.the_client.call_open_api(
+            uri=b'/location/ip',
+            app_id=dumap_test_config.APP_ID,
+            params=params
+        )
+        print(response)
+
+    def test_locate_hardware(self):
+        """
+        test locate hardware
+        """
+
+        body_elem_0 = {}
+        body_elem_0['accesstype'] = 0
+        body_elem_0['imei'] = ''
+        body_elem_0['smac'] = ''
+        body_elem_0['clientip'] = ''
+        body_elem_0['cdma'] = 0
+        body_elem_0['imsi'] = ''
+        body_elem_0['gps'] = ''
+        body_elem_0['network'] = ''
+        body_elem_0['tel'] = ''
+        body_elem_0['bts'] = ''
+        body_elem_0['mmac'] = ''
+        body_elem_0['macs'] = ''
+        body_elem_0['coor'] = ''
+        body_elem_0['output'] = 'JSON'
+        body_elem_0['ctime'] = '1'
+        body_elem_0['need_rgc'] = 'Y'
+
+        body = [body_elem_0]
+
+        params = {}
+        params['src'] = ''
+        params['prod'] = ''
+        params['ver'] = '1.0'
+        params['trace'] = False
+        params['body'] = body
+
+        response = self.the_client.call_open_api(
+            uri=b'/locapi/v2',
+            app_id=dumap_test_config.APP_ID,
+            body=json.dumps(params),
+            method=b'POST'
+        )
+        print(response)
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
@@ -105,5 +162,7 @@ if __name__ == "__main__":
     suite.addTest(TestDumapClient("test_geocoder"))
     suite.addTest(TestDumapClient("test_geoconv"))
     suite.addTest(TestDumapClient("test_direction"))
+    suite.addTest(TestDumapClient("test_locate_ip"))
+    suite.addTest(TestDumapClient("test_locate_hardware"))
     runner = unittest.TextTestRunner()
     runner.run(suite)
