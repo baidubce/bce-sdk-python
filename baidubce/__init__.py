7af49a1771b1223b2361c1b522f7730386bdc28e
lixiaodong06 <lixiaodong06@baidu.com>
2020-11-27 17:54:36 +0800

    BDOS-ES-311;创建支持新旧套餐
    Change-Id: Ife81ce311c3725609819dbfd4b1ac86cebdd02ca

--- baidubce/__init__.py
+++ baidubce/__init__.py
@@ -17,7 +17,7 @@ from builtins import str
 from builtins import bytes
 from . import protocol
 
-SDK_VERSION = b'0.8.53'
+SDK_VERSION = b'0.8.54'
 DEFAULT_SERVICE_DOMAIN = b'bcebos.com'
 URL_PREFIX = b'/v1'
 DEFAULT_ENCODING = 'UTF-8'
