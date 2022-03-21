14c2611b9a8ddb7df8eecf29f21d877a67f9ba52
zhaoxuejiao <zhaoxuejiao@baidu.com>
2022-03-21 15:10:48 +0800

    nvwa-12991 [任务] [KMS] 根据最近KMSPRD开始API和SDK变更
    Change-Id: Ie5fc3660d8ece751c1f530db367cac970842e77d

--- baidubce/__init__.py
+++ baidubce/__init__.py
@@ -17,7 +17,7 @@ from builtins import str
 from builtins import bytes
 from . import protocol
 
-SDK_VERSION = b'0.8.68'
+SDK_VERSION = b'0.8.69'
 DEFAULT_SERVICE_DOMAIN = b'bcebos.com'
 URL_PREFIX = b'/v1'
 DEFAULT_ENCODING = 'UTF-8'
