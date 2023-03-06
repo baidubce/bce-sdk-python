e272a62ed942ec17b65358b78c235cdbda96a677
wangchongshi <wangchongshi@baidu.com>
2023-03-06 13:58:02 +0800

    BCE-JC-23455 [Feature] 【BLB】创建应用型BLB 服务器组端口类型为udp时，需要支持传递udp健康检查字段
    Change-Id: I8e4b5c18794afd72e7218336983ce342fb5b2496

---
+++ sample/blb/app_blb_sample_conf.py
@@ -0,0 +1,26 @@
+# !/usr/bin/env python
+# coding=utf-8
+"""
+Configuration for vpn samples.
+"""
+import logging
+from baidubce.bce_client_configuration import BceClientConfiguration
+from baidubce.auth.bce_credentials import BceCredentials
+
+HOST = b'host'
+AK = b'ak'
+SK = b'sk'
+blbId = b''
+appServerGroupId = ''
+portId = ''
+
+logger = logging.getLogger('baidubce.services.blb.app_blb_client')
+fh = logging.FileHandler('sample.log')
+fh.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+config = BceClientConfiguration(credentials=BceCredentials(AK, SK), endpoint=HOST)
