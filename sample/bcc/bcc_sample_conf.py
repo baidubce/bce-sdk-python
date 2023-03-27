fa7ef397d3061f16eafb8901b1b98bf38ec0e528
wanlinfeng <wanlinfeng@baidu.com>
2023-03-27 20:22:42 +0800

    BCE-JC-23367 [Feature] 20230109【知乎】SDK的文档和样例支持不足- fix code style problem
    Change-Id: Idd44c8a4f3cd7b8102ecd8d9d6e464b4bfe24042

--- sample/bcc/bcc_sample_conf.py
+++ sample/bcc/bcc_sample_conf.py
@@ -19,13 +19,9 @@ import logging
 from baidubce.bce_client_configuration import BceClientConfiguration
 from baidubce.auth.bce_credentials import BceCredentials
 
-# HOST = 'bcc host'
-#AK = 'your-access-key-id'
-# SK = 'your-secret-access-key'
-
-HOST = b'http://bcc.bj.qasandbox.baidu-int.com'
-AK = b'ALTAKjBcN7uGNGaKUEefExLqlS'
-SK = b'eaf64b3f0b06448789bcef8374fc0bc2'
+HOST = 'bcc host'
+AK = 'your-access-key-id'
+SK = 'your-secret-access-key'
 
 logger = logging.getLogger('baidubce.http.bce_http_client')
 fh = logging.FileHandler('sample.log')
