7b544ba15716dd6fe7c52c66833f23f5f1e2b1b3
zhoujian12 <zhoujian12@baidu.com>
2023-12-06 14:40:19 +0800

    VPC-5381 modify eni sdk according to CR
    Change-Id: Ib82e5d8c0c8d7e31db9a174c433be0cf5394997d

--- baidubce/services/eni/examples/example_create_eni.py
+++ baidubce/services/eni/examples/example_create_eni.py
@@ -12,16 +12,17 @@ from baidubce.services.eni import eni_model
 if __name__ == '__main__':
     ak = "Your Ak"  # 账号的Ak
     sk = "Your Sk"  # 账号的Sk
-    endpoint = "Your endpoint"  # 服务对应的Region域名
+    endpoint = "bcc.bj.baidubce.com"  # 服务对应的Region域名, 例如bj Region域名
     config = BceClientConfiguration(credentials=BceCredentials(access_key_id=ak, secret_access_key=sk),
                                     endpoint=endpoint)
     client = eni_client.EniClient(config)  # client 初始化
-    eni_ip_list = [eni_model.EniIPSet(public_ip="", private_ip="10.0.1.111", primary=True), # IP地址列表
-                   eni_model.EniIPSet(public_ip="", private_ip="10.0.1.112", primary=False)]
+    
     try:
-        resp = client.create_eni(name="PYTHON-SDK-TEST", subnet_id="sbn-d63m7t0bbwt5", 
-                                     security_group_ids=["g-92600fd1grhr"],
-                                     eni_ip_address_list=eni_ip_list)  # 创建eni
-        print("create eni response :%s", resp)
+        eni_ip_list = [eni_model.EniIPSet(public_ip="", private_ip="10.0.1.115", primary=True), 
+                       eni_model.EniIPSet()]    # IP地址列表, EniIPSet不指定private_ip默认分配可用的内网IP
+        resp = client.create_eni(name="eni-1", subnet_id="sbn-d63m7t0bbwt5", 
+                                 security_group_ids=["g-92600fd1grhr"],
+                                 eni_ip_address_list=eni_ip_list)  # 创建eni
+        print("create eni response :%s" % resp)
     except BceHttpClientError as e:
         print("Exception when calling api: %s\n" % e)
--- baidubce/services/eni/examples/example_delete_eni.py
+++ baidubce/services/eni/examples/example_delete_eni.py
@@ -10,13 +10,13 @@ from baidubce.services.eni import eni_client
 if __name__ == '__main__':
     ak = "Your Ak"  # 账号的Ak
     sk = "Your Sk"  # 账号的Sk
-    endpoint = "Your endpoint"  # 服务对应的Region域名
+    endpoint = "bcc.bj.baidubce.com"  # 服务对应的Region域名, 例如bj Region域名
     config = BceClientConfiguration(credentials=BceCredentials(access_key_id=ak, secret_access_key=sk),
                                     endpoint=endpoint)  
     client = eni_client.EniClient(config)  # client 初始化
     try:
         eni_id = "eni-c6adp40kpick"       # 要删除的eni id
         resp = client.delete_eni(eni_id)  # 删除一个eni
-        print("delete eni response :%s", resp)
+        print("delete eni response :%s" % resp)
     except BceHttpClientError as e:
         print("Exception when calling api: %s\n" % e)
