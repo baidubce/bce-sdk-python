da5ec77c1b9033de240c6f95143d1896f6c297f6
yangtao24 <yangtao24@baidu.com>
2023-12-21 20:12:47 +0800

    [VPC-5465]Feat.专线SDK
    Change-Id: I76864a6e681d6c3ca30fbbb0ae2c4b5597a52d1f

---
+++ sample/et/example_create_et_channel.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+example for create Et channel
+"""
+
+import uuid
+
+from baidubce.auth.bce_credentials import BceCredentials
+from baidubce.bce_client_configuration import BceClientConfiguration
+from baidubce.exception import BceHttpClientError
+from baidubce.services.et import et_client
+
+if __name__ == "__main__":
+    ak = "Your AK"    #用户的ak
+    sk = "Your SK"    #用户的sk
+    endpoint = 'Your endpoint'  # 服务器对应的Region域名
+    config = BceClientConfiguration(credentials=BceCredentials(access_key_id=ak, secret_access_key=sk),
+                                    endpoint=endpoint)
+    et_client = et_client.EtClient(config)
+    try:
+        resp = et_client.create_et_channel(et_id="Your Et Id",
+                                           local_ip="Your BaiduAddress",
+                                           name="Your Channel Name",
+                                           remote_ip="Your CustomerAddress",
+                                           route_type="Your RouteType",
+                                           vlan_id=100,
+                                           client_token=str(uuid.uuid4()))
+        print("create et channel response: %s", resp.id)
+    except BceHttpClientError as e:
+        print("Exception when calling api: %s" % e)
