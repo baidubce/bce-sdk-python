8ffd5149c8ba7fa5cf49635d30c3d13ada4b68fb
zhangwenqi02 <zhangwenqi02@baidu.com>
2023-12-26 21:51:17 +0800

    VPC-5380 et channel route rule&disable ipv6
    Change-Id: Ia41c1e51484fbe46aa4dee4ff3d749380d6b0da0

--- sample/et/example_list_et_channel_route_rule.py
+++ sample/et/example_list_et_channel_route_rule.py
@@ -5,7 +5,7 @@ example for et channel route rule.
 from baidubce.auth.bce_credentials import BceCredentials
 from baidubce.bce_client_configuration import BceClientConfiguration
 from baidubce.exception import BceHttpClientError
-from baidubce.services.et import et_channel_route_rule_client as client
+from baidubce.services.et import et_client
 
 if __name__ == '__main__':
     ak = "Your Ak"  # 账号的Ak
@@ -13,13 +13,13 @@ if __name__ == '__main__':
     endpoint = "bcc.bj.baidubce.com"  # 服务对应的Region域名
     config = BceClientConfiguration(credentials=BceCredentials(access_key_id=ak, secret_access_key=sk),
                                     endpoint=endpoint)
-    client = client.EtChannelRouteRuleClient(config)  # client 初始化
-    
-    et_id = "etId"  # 专线ID
-    et_channel_id = "etChannelId"  # 专线通道ID
-    
+    client = et_client.EtClient(config)
+ 
     try:
-        resp = client.list_et_channel_route_rules(et_id, et_channel_id)  # 查询专线通道路由规则
-        print("[example] list et channel route rule response: %s." % resp)
+        resp = client.list_et_channel_route_rules(et_id="Your etId",
+                                                  et_channel_id="Your etChannelId",
+                                                  max_Keys=10,
+                                                  dest_address="192.168.0.7/32")
+        print("list et channel route rule response: %s." % resp)
     except BceHttpClientError as e:
         print("Exception when calling api: %s.\n" % e)
