08a5cdd0c273cd3d2228d9792a51d1c5750aa93b
wanghongxiang <wanghongxiang@baidu.com>
2024-01-05 16:30:40 +0800

    IaaS-BLB-2408: new-add api and example for application blb
    Change-Id: I5d59f34461b60475b7573ab4d1b9818cd2c2b290

--- sample/appblb/example_update_app_ip_group_port.py
+++ sample/appblb/example_update_app_ip_group_port.py
@@ -1,7 +1,16 @@
-# !/usr/bin/env python
-# coding=utf-8
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Baidu.com, Inc. All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
 """
-Samples for app blb client.
+example for update app ip group port.
 """
 
 from baidubce.bce_client_configuration import BceClientConfiguration
@@ -23,10 +32,10 @@ if __name__ == "__main__":
     # create an app blb client
     app_blb_client = AppBlbClient(config)
 
-    blb_id = "lb-a889d7d4"
-    ip_group_id = "ip_group-21475a51"
-    port = "ip_group_policy-3361c4b5"
-    health_check_down_retry = 5
+    blb_id = "lb-xxxxxxxx"  # 指定的BLB ID
+    ip_group_id = "ip_group-xxxxxxxx"  # IP组ID
+    port = "ip_group_policy-xxxxxxxx"  # 端口协议ID
+    health_check_down_retry = 5  # 健康检查失败重试次数
 
     # update ip group port
     try:
