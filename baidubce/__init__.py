198b1a8fde01bb19bb8fbfd1e7a30c7582ec0104
pangyangyang <pangyangyang@baidu.com>
2024-01-15 15:35:46 +0800

    CloudWatch-10620 add version
    Change-Id: Ie8391a54befe260b52f6f18c816fbe841ba9b0e4

--- baidubce/__init__.py
+++ baidubce/__init__.py
@@ -18,7 +18,7 @@ from builtins import bytes
 from . import protocol
 
 
-SDK_VERSION = b'0.9.02'
+SDK_VERSION = b'0.9.03'
 DEFAULT_SERVICE_DOMAIN = b'bcebos.com'
 URL_PREFIX = b'/v1'
 DEFAULT_ENCODING = 'UTF-8'
