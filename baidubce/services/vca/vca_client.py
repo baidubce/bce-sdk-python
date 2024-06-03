dff240f622b1be5775e6d6150705372539d5b40c
heke03 <heke03@baidu.com>
2024-06-03 20:02:11 +0800

    acg-aibps-videoai-13526 [Task] 【MCA公有云】SDK新增取消接口 Merge branch 'master' into mca
    Change-Id: Ice8f9acd1a4e17b6757f121d9ce1d6a6571ff8f5

--- baidubce/services/vca/vca_client.py
+++ baidubce/services/vca/vca_client.py
@@ -57,6 +57,16 @@ class VcaClient(BceBaseClient):
         return self._send_request(http_methods.PUT, b'/v2/image', body=json.dumps(body), params={b'sync': ''})
 
     @required(source=(bytes, str))
+    def cancel(self, source):
+        """
+        cancel a media analyze by source
+        :param source: media source
+        :type source: string or unicode
+        :return: **Http Response**
+        """
+        return self._send_request(http_methods.PUT, b'/v2/media', params={b'source': source, b'cancel': ''})
+
+    @required(source=(bytes, str))
     def put_media(self, source, preset=None, notification=None, priority=None,
                   title=None, sub_title=None, category=None,
                   description=None, config=None):
