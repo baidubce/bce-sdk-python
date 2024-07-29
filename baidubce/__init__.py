280497526e8bc5369a0229368a0343a33109dfd8
yanyuyu <yanyuyu@baidu.com>
2024-07-29 16:50:57 +0800

    CloudWatch-12263 【云助手】提供SDK
    Change-Id: I544897e6ff55fb57b4d0b8e33ad9475604645bd7

--- baidubce/__init__.py
+++ baidubce/__init__.py
@@ -18,7 +18,7 @@ from builtins import bytes
 from . import protocol
 
 
-SDK_VERSION = b'0.9.18'
+SDK_VERSION = b'0.9.19'
 DEFAULT_SERVICE_DOMAIN = b'bcebos.com'
 URL_PREFIX = b'/v1'
 DEFAULT_ENCODING = 'UTF-8'
