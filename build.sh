e2a08cddbc872fddaf45dc541c1eb632c789fcc8
LuChao <atorber@163.com>
2024-09-01 16:07:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ TARGET="bce-python-sdk-$VERSION"
 mkdir $TARGET
 cp -rf baidubce sample setup.py README.txt $TARGET
 zip -r ${TARGET}.zip $TARGET -x "*.pyc" "*.pyo"
-cp ${TARGET}.zip output/
+# mv -fv ${TARGET}.zip output/
 
 PYTHON=python
 [ -f /home/cmc/opt/pyenv/versions/2.7.14/bin/python2.7 ] && PYTHON=/home/cmc/opt/pyenv/versions/2.7.14/bin/python2.7
@@ -17,4 +17,4 @@ echo "using Python=$PYTHON"
 
 $PYTHON setup.py bdist_wheel --universal
 $PYTHON setup.py sdist
-mv -fv dist/* output/dist/
+cp dist/* output/dist/
