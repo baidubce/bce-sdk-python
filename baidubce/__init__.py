c2c75f7737399d4d481d786c4e3e9c982e52bcb8
atorber <atorber@163.com>
2024-09-10 11:17:18 +0800

    100.9.19.5

--- baidubce/__init__.py
+++ baidubce/__init__.py
@@ -18,7 +18,7 @@ from builtins import bytes
 from . import protocol
 
 
-SDK_VERSION = b'100.9.19.4'
+SDK_VERSION = b'100.9.19.5'
 DEFAULT_SERVICE_DOMAIN = b'bcebos.com'
 URL_PREFIX = b'/v1'
 DEFAULT_ENCODING = 'UTF-8'
--- baidubce/services/aihc/aihc_client.py
+++ baidubce/services/aihc/aihc_client.py
@@ -6,7 +6,8 @@ This module provides a client class for TSDB.
 
 import copy
 import json
-# import logging
+import os
+import logging
 from baidubce.auth import bce_v1_signer
 from baidubce.bce_base_client import BceBaseClient
 from baidubce.http import bce_http_client
@@ -15,11 +16,52 @@ from baidubce.http import http_content_types
 from baidubce.http import http_headers
 from baidubce.http import http_methods
 from baidubce.services.aihc import aihc_handler
-from baidubce.services.aihc import job_chain
 from baidubce.services.aihc import generate_aiak_parameter
 
 
 # _logger = logging.getLogger(__name__)
+# 配置日志
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+def load_config(config_file):
+    if not os.path.exists(config_file):
+        raise FileNotFoundError(f"File {config_file} not found.")
+    with open(config_file, 'r') as f:
+        return json.load(f)
+
+
+def validate_index(index, jobs_count):
+    if index < 0 or index >= jobs_count:
+        raise IndexError(f"Index {index} is out of range.")
+
+
+def build_command(job_info, config_dir, scrips_path, config_path,
+                  index, jobs_count):
+    command = job_info['jobSpec']['command']
+    if (command.endswith('.sh') and
+        len(command.split('.')) == 2 and
+            command.startswith('bash') is not True):
+        command_path = os.path.join(config_dir, command)
+        if not os.path.exists(command_path):
+            raise FileNotFoundError(f"File {command_path} not found.")
+
+        with open(command_path, 'r') as f:
+            command = f.read()
+
+    if index != jobs_count - 1:
+        command += (
+            '\n' + 'echo "job_chain:The previous task has been completed."'
+            '\n' + 'pip install future'
+            '\n' + 'pip install pycryptodome'
+            '\n' + 'pip install bce-python-sdk-next'
+            '\n' + 'echo "job_chain:Next job is to be continued..."'
+        )
+        next_command = f'python {scrips_path} {config_path} {index + 1}'
+        command += f'\n{next_command}'
+
+    return command
 
 
 class AIHCClient(BceBaseClient):
@@ -29,9 +71,6 @@ class AIHCClient(BceBaseClient):
 
     def __init__(self, config=None):
         BceBaseClient.__init__(self, config)
-
-    def create_job_chain(self, config_file=None, index=None):
-        return job_chain.create_job_chain(config_file, index)
 
     def generate_aiak_parameter(self, chain_job_config=None, aiak_job_config=None):
         return generate_aiak_parameter.generate_aiak_parameter(chain_job_config, aiak_job_config)
@@ -52,6 +91,44 @@ class AIHCClient(BceBaseClient):
         return self._send_request(http_methods.POST, path=path, body=body,
                                   params=params,
                                   body_parser=aihc_handler.parse_json)
+
+    def create_job_chain(self, config_file=None, index=None):
+        try:
+            job_info = load_config(config_file)
+            jobs = job_info['jobs']
+            api_config = job_info['api_config']
+            resourcePoolId = job_info['resourcePoolId']
+            scrips_path = job_info['scrips_path']
+            config_path = job_info['config_path']
+
+            validate_index(index, len(jobs))
+
+            config_dir = os.path.dirname(config_file)
+            command = build_command(jobs[index], config_dir, scrips_path,
+                                    config_path, index, len(jobs))
+            jobs[index]['jobSpec']['command'] = command
+
+            logging.info("Job info at index retrieved successfully.")
+            logging.info(json.dumps(jobs[index]))
+
+            logging.info("Creating AI job using openapi...")
+
+            cur_job_info = jobs[index]
+
+            result = self.create_ai_job(api_config, resourcePoolId, cur_job_info)
+            tasks_url = 'https://console.bce.baidu.com/aihc/tasks'
+            print('====================================\n')
+            logging.info('任务创建结果: %s', result)
+            logging.info('查看任务列表: https://console.bce.baidu.com/aihc/tasks')
+            print('\n====================================')
+            return {
+                result: result,
+                tasks_url: tasks_url
+            }
+        except (FileNotFoundError, IndexError, json.JSONDecodeError) as e:
+            logging.error("Error: %s", e)
+        except Exception as e:
+            logging.error("An unexpected error occurred: %s", e)
 
     def delete_aijob(self, aijob_id):
         """
