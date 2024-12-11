3345ec65eeef93767eaefe6c7b080539ea80da34
liguozhong <liguozhong@baidu.com>
2024-12-11 16:38:40 +0800

    VPC-7195 improve dns list record
    Change-Id: I20eb1704a2813f7df6c1aa04e206e1823ce23313

---
+++ sample/et/example_disassociate_et_channel.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+example for disassociate et channel.
+"""
+
+import uuid
+
+from baidubce.auth.bce_credentials import BceCredentials
+from baidubce.bce_client_configuration import BceClientConfiguration
+from baidubce.exception import BceHttpClientError
+from baidubce.services.et import et_client
+
+
+if __name__ == "__main__":
+    ak = "Your AK"
+    sk = "Your SK"
+    endpoint = "bcc.bj.baidubce.com"
+    config = BceClientConfiguration(credentials=BceCredentials(access_key_id=ak, secret_access_key=sk),
+                                    endpoint=endpoint)
+    et_client = et_client.EtClient(config)
+    et_id = "Your et_id"
+    et_channel_id = "Your et_channel_id"
+    extra_channel_id = "Your extra_channel_id"
+    try:
+        resp = et_client.disassociate_et_channel(et_id=et_id, et_channel_id=et_channel_id,
+                                                 extra_channel_id=extra_channel_id, client_token=str(uuid.uuid4()))
+        print("Disassociate et channel response: %s" % resp)
+    except BceHttpClientError as e:
+        print("Exception when calling api: %s" % e)
